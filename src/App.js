e17b451ce418cebbf83decabce58b25756b75c09
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2023-09-09 14:34:32 +0530

    popup-gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Media } from './Components';
+import {useState } from 'react'
+// import { ReactVideoGallery } from './Components/ReactVideoGallery';
+// import { VideoCarousel } from './Components/VideoCarousel';
+
+
+
 
 function App() {
+
+  const [file, setFile]=useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Video Galley</h1>
+      <div className='media-container'>
+        {
+          Media.map((file, index)=>(
+            <div className="media" key={index} 
+            onClick={()=>setFile(file)}>
+              {
+                <video src={file.url}></video>
+                
+              }
+
+            </div>
+          ))
+        }
+      </div>
+
+      <div className='popup-media' style={{display:file?'block' : 'none'}}>
+          <span onClick={()=>setFile(null)}>&times;</span>
+          {
+            <video src={file?.url} muted autoPlay controls></video>
+          }
+      </div>
     </div>
   );
 }
---
+++ src/Components/index.js
@@ -0,0 +1,87 @@
+
+
+export const Media = [
+    {
+        type: 'video',
+        url: require('./Videos/video1.mp4')
+    },
+
+
+    {
+        type: 'video',
+        url: require('./Videos/video2.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video6.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video7.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video8.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video9.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video10.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video11.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video4.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video12.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video13.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video14.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video15.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video16.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video4.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video17.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video4.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video18.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video19.mp4')
+    }
+    
+]
